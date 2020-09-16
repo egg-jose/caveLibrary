4c4f87ec3c99f4a464be44a243f240f5ded29039
Jose Esteban Garcia Grullon <30965582+jose1897@users.noreply.github.com>
2020-09-15 23:48:35 -0400

    Add prettierrc config

--- models/Book.js
+++ models/Book.js
@@ -1,71 +1,62 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const Joi = require('joi');
 //MongoDB Schema for the books
 const bookSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"]
-  },
-  datePublished: Date,
-  authorName: {
-    type: String,
-    required: [true, "Author name's is required"],
-  },
-  amountPages: {
-    type: Number,
-    min: [1, "The number of pages must be greater than 0"],
-  },
-  genres: [
-    {
-      type: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
     },
-  ],
-  isBestSeller: Boolean,
+    datePublished: Date,
+    authorName: {
+        type: String,
+        required: [true, 'Author name is required'],
+    },
+    amountPages: {
+        type: Number,
+        min: [1, 'The number of pages must be greater than 0'],
+    },
+    genres: [
+        {
+            type: String,
+        },
+    ],
+    isBestSeller: Boolean,
 });
 
-bookSchema.pre("save", function (next) {
-  const { error } =joiValidate.call(this);
-  if(!error) {
-    throw new Error(error);
-  }
-  next();
-});
-
-function joiValidate () {
-  //Setting maximum validation date for datePublished
-  let validationDate = new Date();
-  validationDate.setMonth(validationDate.getMonth() + 3);
-  //Function to determine that the elements of the array are unique
-  const areArrayItemsUnique = (value, helpers) => {
-    if (value.length == [...new Set(value)].length) {
-      return value;
+bookSchema.pre('save', function (next) {
+    const { error } = joiValidate.call(this);
+    if (!error) {
+        throw new Error(error);
     }
-    throw new Error('Genres must be unique');
+    next();
+});
 
-  }
+function joiValidate() {
+    //Setting maximum validation date for datePublished
+    let validationDate = new Date();
+    validationDate.setMonth(validationDate.getMonth() + 3);
+    //Function to determine that the elements of the array are unique
+    const areArrayItemsUnique = (value, helpers) => {
+        if (value.length == [...new Set(value)].length) {
+            return value;
+        }
+        throw new Error('Genres must be unique');
+    };
 
-  let schema = Joi.object({
-    name: Joi.string()
-                    .required(),
-    datePublished: Joi.date()
-                                  .max(validationDate),
-    authorName: Joi.string()
-                              .regex(/^[A-Za-z0-9]+$/)
-                              .required(),
-    amountPages: Joi.number()
-                                .integer()
-                                .min(1),
-    genres: Joi.array()
-                      .items(
-                        Joi.string()
-                      )
-                      .custom(areArrayItemsUnique, 'Unique items validation'),
-    isBestSeller: Joi.boolean()
-  });
-  return schema.validate(this);
-};
-
+    let schema = Joi.object({
+        name: Joi.string().required(),
+        datePublished: Joi.date().max(validationDate),
+        authorName: Joi.string()
+            .regex(/^[A-Za-z0-9]+$/)
+            .required(),
+        amountPages: Joi.number().integer().min(1),
+        genres: Joi.array()
+            .items(Joi.string())
+            .custom(areArrayItemsUnique, 'Unique items validation'),
+        isBestSeller: Joi.boolean(),
+    });
+    return schema.validate(this);
+}
 
 let Book = mongoose.model('Book', bookSchema);
 
